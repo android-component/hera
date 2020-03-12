39add553d323d98f2cc3edd1f332c2c0e502fd03
tangqi <tangqi@mgtv.com>
2020-03-12 13:05:19 +0800

    Modify app style

--- android/sample/src/main/java/com/weidian/lib/hera/sample/SampleActivity.java
+++ android/sample/src/main/java/com/weidian/lib/hera/sample/SampleActivity.java
@@ -1,11 +1,15 @@
 package com.weidian.lib.hera.sample;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.weidian.lib.hera.main.HeraService;
+import com.weidian.lib.hera.utils.StorageUtil;
+
+import java.io.File;
 
 public class SampleActivity extends AppCompatActivity {
 
@@ -14,17 +18,40 @@ public class SampleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sample_activity);
 
-        final String userId = "123";//标识宿主App业务用户id
-        final String appId = "app";//小程序的id
-        final String appPath = "";//小程序的本地存储路径
-        //sdk内部会首先读取并解压appPath下的小程序包，若appPath为空，则读取并解压assets下以appId命名的zip文件
-        //小程序解压后将存储在以appId命名的文件夹下
         findViewById(R.id.enter_hera).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                HeraService.launchHome(getApplicationContext(), userId, appId, appPath);
+                final String appId = "mgtv-app";//小程序的id
+                launchHome(appId);
             }
         });
     }
 
+    /**
+     * 启动首页
+     * @param appId
+     */
+    private void launchHome(String appId) {
+        //小程序的本地存储路径
+        //sdk内部会首先读取并解压appPath下的小程序包，若appPath为空，则读取并解压assets下以appId命名的zip文件
+        //小程序解压后将存储在以appId命名的文件夹下
+        final String userId = "123";//标识宿主App业务用户id
+        final String appPath = getMiniAppPath(this, appId);
+        HeraService.launchHome(getApplicationContext(), userId, appId, appPath);
+    }
+
+    /**
+     * 获取指定的小程序目录
+     *
+     * @param context 上下文
+     * @param appId   小程序id
+     * @return 指定小程序id的目录文件
+     */
+    public static String getMiniAppPath(Context context, String appId) {
+        File miniAppDir = new File(StorageUtil.getHeraAppPath(context), appId);
+        if (!miniAppDir.exists() || !miniAppDir.isDirectory()) {
+            return null;
+        }
+        return miniAppDir.getAbsolutePath();
+    }
 }
